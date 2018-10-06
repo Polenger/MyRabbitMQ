210a3b6fa4c89ad5ab2c47f15630bd745c2119a4
polenger <polenger@qq.com>
2018-10-06 22:57:55 +0800

    采用默认设置

--- RabbitMQ01/src/main/java/com/loikun/work/NewTask.java
+++ RabbitMQ01/src/main/java/com/loikun/work/NewTask.java
@@ -10,7 +10,6 @@ public class NewTask {
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
         channel.queueDeclare(TASK_QUEUE_NAME, false,false, false, null);
