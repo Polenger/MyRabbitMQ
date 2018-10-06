4e2776beda6202d4313018bf40383abb253f5d7d
polenger <polenger@qq.com>
2018-10-06 21:25:45 +0800

    路由转发

---
+++ RabbitMQ01/src/main/java/com/loikun/rounting/ReceiveLogsDirect2.java
@@ -0,0 +1,43 @@
+package com.loikun.rounting;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class ReceiveLogsDirect2 {
+    private static final String EXCHANGE_NAME = "direct_logs";
+
+    private static final String[] routingKeys = new String[]{"info", "warning"};
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        // 声明交换机
+        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+        // 获取队列名称
+        String queue = channel.queueDeclare().getQueue();
+        // 根据路由器关键字进行绑定
+        // 怎么绑定？一次遍历
+        for (String routingKey : routingKeys) {
+            channel.queueBind(queue, EXCHANGE_NAME, routingKey);
+            //
+            System.out.println("ReceiveLogsDirect2 exchange:"+ EXCHANGE_NAME+","
+            +"queue:"+queue+", BindRoutingKey:"+routingKey);
+        }
+        System.out.println("ReceiveLogsDirect2 Wating for message");
+        Consumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag,
+                                       Envelope envelope,
+                                       AMQP.BasicProperties properties,
+                                       byte[] body) throws IOException {
+                System.out.println("ReceiveLogsDirect2 Received '"
+                        +envelope.getRoutingKey()+"':'"+new String(body,"UTF-8")+"'");
+            }
+        };
+        channel.basicConsume(queue,true, consumer);
+
+    }
+}
---
+++ RabbitMQ01/src/main/java/com/loikun/rounting/RoutingSendDirect.java
@@ -0,0 +1,28 @@
+package com.loikun.rounting;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class RoutingSendDirect {
+    private static final String EXCHANGE_NAME = "direct_logs";
+    private static final String[] rountingKeys = new String[]{"info","warning","error","debug"};
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        // 声明交换机
+        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+        for (String routingKey : rountingKeys) {
+            String message = "RountingSendDirect send the message level : " + routingKey;
+            channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes());
+            System.out.println("RoutingSendDirect Send"+routingKey +"':'" + message);
+        }
+        channel.close();
+        connection.close();
+    }
+}
