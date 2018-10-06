b85a036a5b00a59f7ec2e66de107636d35e9acb8
polenger <polenger@qq.com>
2018-10-06 19:49:00 +0800

    任务分发实现例子

---
+++ RabbitMQ01/src/main/java/com/loikun/work/NewTask.java
@@ -0,0 +1,27 @@
+package com.loikun.work;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class NewTask {
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(TASK_QUEUE_NAME, false,false, false, null);
+
+        for (int i = 0; i<10; i++){
+            String message = "hello RabbitMQ "+i;
+            channel.basicPublish("", TASK_QUEUE_NAME,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,message.getBytes());
+            System.out.println("NewTask send '"+message+"'");
+        }
+        channel.close();
+        connection.close();
+    }
+}
---
+++ RabbitMQ01/src/main/java/com/loikun/work/Worker2.java
@@ -0,0 +1,48 @@
+package com.loikun.work;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+public class Worker2 {
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+        System.out.println("Worker2  Waiting for messages");
+        channel.basicQos(1);
+        Consumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag,
+                                       Envelope envelope,
+                                       AMQP.BasicProperties properties,
+                                       byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println("worker2  Received '" + message + "'");
+                try {
+                    //throw new Exception();
+                    doWork(message);
+                }catch (Exception e){
+                    channel.abort();
+                }finally {
+                    System.out.println("worker2 Done");
+                    channel.basicAck(envelope.getDeliveryTag(),false);
+                }
+            }
+        };
+        boolean autoAck = false;
+        channel.basicConsume(TASK_QUEUE_NAME, autoAck, consumer);
+    }
+    private static void doWork(String task) {
+        try {
+            Thread.sleep(1000); // 暂停1秒钟
+        } catch (InterruptedException _ignored) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+}
