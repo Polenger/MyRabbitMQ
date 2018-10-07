b18cc0054088f9619b32773a4565a8378d67c87b
polenger <polenger@qq.com>
2018-10-07 19:42:31 +0800

    routing direct

---
+++ RabbitMQ02/src/main/java/com/loikun/spring/Routing/RoutSender.java
@@ -0,0 +1,39 @@
+package com.loikun.spring.Routing;
+
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("rout")
+public class RoutSender {
+
+    @Autowired
+    private RabbitTemplate template;
+
+    @Autowired
+    private DirectExchange direct;
+
+    private int index;
+
+    private int count;
+
+    private final String[] keys = {"orange", "black", "green"};
+
+    @GetMapping("send")
+    public void send() {
+        StringBuilder builder = new StringBuilder("Hello to ");
+        if (++this.index == 3) {
+            this.index = 0;
+        }
+        String key = keys[this.index];
+        builder.append(key).append(' ');
+        builder.append(Integer.toString(++this.count));
+        String message = builder.toString();
+        template.convertAndSend(direct.getName(), key, message);
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+}
